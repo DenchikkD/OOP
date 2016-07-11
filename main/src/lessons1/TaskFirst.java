85b4dc0bf54ff3448f51edb546e5c86b63b2c1d1
Denni_Asus <Online08.08@mail.ru>
2016-07-11 20:29:40 +0300

    lessons1

---
+++ main/src/lessons1/TaskFirst.java
@@ -0,0 +1,12 @@
+package lessons1;
+
+/**
+ * Created by Denni on 11.07.2016.
+ */
+public class TaskFirst {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        
+        System.out.println(array.length);
+    }
+}
