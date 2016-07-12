90f13192d419ca912ca19c5aa95c98e1ea1d02b4
Denni_Asus <Online08.08@mail.ru>
2016-07-12 19:35:33 +0300

    lessons1+txt+

--- main/src/lessons1/Task2.java
+++ main/src/lessons1/Task2.java
@@ -2,6 +2,7 @@ package lessons1;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Denni on 11.07.2016.
@@ -9,17 +10,21 @@ import java.io.IOException;
 public class Task2 {
     public static void main(String[] args) throws IOException {
 
-        FileWriter writer = new FileWriter("D:\\progects/OOP/main/FileDirectory/text.txt");// якщо з true то файл дозаписується
-        for (int i = 0; i <1000 ; i++) {
-            writer.write("Hello word");
-            writer.write(System.lineSeparator());
+//        FileWriter writer = new FileWriter("D:\\progects/OOP/main/FileDirectory/text.txt");// якщо з true то файл дозаписується
+//        for (int i = 0; i <1000 ; i++) {
+//            writer.write("Hello word");
+//            writer.write(System.lineSeparator());
+//
+//        }
+        PrintWriter printwriter = new PrintWriter("D:\\progects/OOP/main/FileDirectory/text.txt");// якщо з true то файл дозаписується
+        for (int i = 0; i < 10000; i++) {
+            printwriter.println("Hello word");
+            printwriter.flush();
 
         }
 //        writer.flush();
 //        writer.close();
 //        writer.write("c");// уже не можна використовувати, оскільки поток записі закритий методом close
-
-
 
 
     }
